1958878a75553c40b7b8f916a515215f58bf61ef
0xsaju <mdshossain96@gmail.com>
2025-07-23 18:44:42 +0600

    web apps service disabled & vm service enabled thru tf

--- infra/main.tf
+++ infra/main.tf
@@ -38,47 +38,119 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all" {
   end_ip_address      = "255.255.255.255"
 }
 
-resource "azurerm_service_plan" "main" {
-  name                = "myapp-service-plan"
+# Add networking resources for the VM
+resource "azurerm_public_ip" "main" {
+  name                = "myapp-vm-ip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+  allocation_method   = "Static"
 }
 
-resource "azurerm_linux_web_app" "backend" {
-  name                = "myapp-backend-2024"
+resource "azurerm_virtual_network" "main" {
+  name                = "myapp-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  service_plan_id     = azurerm_service_plan.main.id
-  site_config {
-    application_stack {
-      node_version = "18-lts"
-    }
-  }
-  app_settings = {
-    DB_HOST     = azurerm_mysql_flexible_server.main.fqdn
-    DB_USER     = "appuser@myappmysql2024"
-    DB_PASSWORD = "appPassword123!"
-    DB_NAME     = "appdb"
-    DB_PORT     = "3306"
-    JWT_SECRET  = "supersecretkey"
-    FRONTEND_URL = "https://myapp-frontend-2024.azurewebsites.net"
-    PORT        = "4000"
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "myapp-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "myapp-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
-resource "azurerm_linux_web_app" "frontend" {
-  name                = "myapp-frontend-2024"
+resource "azurerm_network_security_group" "main" {
+  name                = "myapp-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  service_plan_id     = azurerm_service_plan.main.id
-  site_config {
-    application_stack {
-      node_version = "18-lts"
-    }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
-  app_settings = {
-    REACT_APP_API_URL = "https://${azurerm_linux_web_app.backend.default_hostname}"
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "Frontend3000"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Backend4000"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "4000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "myapp-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/Users/sazzad/.ssh/id_rsa.pub")
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+  custom_data = filebase64("${path.module}/cloud-init.txt")
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.main.ip_address
 } 
